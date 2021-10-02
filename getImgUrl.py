e4fd45f01636199b752aa26781e9c9259438ce31
tvkoz <tv.kozlov.2002@gmail.com>
2021-10-02 15:35:45 +0300

    Прототип функции поиска фоток

---
+++ getImgUrl.py
@@ -0,0 +1,5 @@
+def getImgUrl(query):
+    query= query.split()
+    query='+'.join(query)
+    url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
+    return url
--- image_search.py
+++ image_search.py
@@ -1,1 +1,15 @@
-# Тут функция поиска картинок def image_search(phrase):
+import requests
+from request_word import get_url
+from bs4 import BeautifulSoup
+from getImgUrl import getImgUrl
+
+def image_search(query, count=5):
+    response = requests.get(getImgUrl(query))
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    quotes = soup.find_all('img')
+
+    for i in range(1, count + 1):
+        print(quotes[i]['src'])
+
+image_search("Закат")
