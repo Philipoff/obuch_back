e1723aa5d74eac19f35fdbda02243fe1f3fc1055
tvkoz <tv.kozlov.2002@gmail.com>
2021-10-03 03:42:11 +0300

    Альфа функция парсинга картинок

--- image_search.py
+++ image_search.py
@@ -1,15 +1,26 @@
-import requests
-from request_word import get_url
-from bs4 import BeautifulSoup
+from selenium import webdriver
 from getImgUrl import getImgUrl
+from time import sleep
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+options = webdriver.ChromeOptions()
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+options.headless = True
+main_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+additional_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+
 
 def image_search(query, count=5):
-    response = requests.get(getImgUrl(query))
-    soup = BeautifulSoup(response.text, 'lxml')
+    main_driver.get(getImgUrl(query))
+    elems = main_driver.find_elements_by_xpath('//a[contains(@class, "wXeWr islib nfEiy")]')
+    for i in range(0, count):
+        elems[i].click()
+        additional_driver.get(elems[i].get_attribute('href'))
+        print(additional_driver.find_element_by_xpath('//img[contains(@class, "n3VNCb")]').get_attribute("src"))
+    return 1
 
-    quotes = soup.find_all('img')
-
-    for i in range(1, count + 1):
-        print(quotes[i]['src'])
-
-image_search("Закат")
+image_search("Котики")
+main_driver.quit()
+additional_driver.quit()
