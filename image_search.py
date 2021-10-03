94181f710539bc614dbc79bce23441e64e60745b
tvkoz <tv.kozlov.2002@gmail.com>
2021-10-03 04:49:52 +0300

    Функции парсинга: картинки и видосы

--- image_search.py
+++ image_search.py
@@ -5,25 +5,33 @@ from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-options = webdriver.ChromeOptions()
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-options.headless = True
-main_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
-additional_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
-
 
 def image_search(query, count=5):
+    options = webdriver.ChromeOptions()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    options.headless = True
+    main_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+    additional_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+
     main_driver.get(getImgUrl(query))
     sleep(0.2)
     elems = main_driver.find_elements_by_xpath('//a[contains(@class, "wXeWr islib nfEiy")]')
-    for i in range(0, count):
-        elems[i].click()
-        additional_driver.get(elems[i].get_attribute('href'))
-        soup = BeautifulSoup(additional_driver.page_source, "lxml")
-        print(soup.find('img', class_="n3VNCb")["src"])
-    return 1
 
-image_search("Котики")
-main_driver.quit()
-additional_driver.quit()
+    res = []
+    for i in range(0, min(count, len(elems))):
+        try:
+            elems[i].click()
+            additional_driver.get(elems[i].get_attribute('href'))
+            soup = BeautifulSoup(additional_driver.page_source, "lxml")
+            res.append(soup.find('img', class_="n3VNCb")["src"])
+        except:
+            pass
+
+    main_driver.quit()
+    additional_driver.quit()
+
+    return res
+
+
+#print(image_search("котики"))
--- video_search.py
+++ video_search.py
@@ -5,21 +5,28 @@ from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-options = webdriver.ChromeOptions()
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-options.headless = True
-main_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
 
 def video_search(query, count=5):
+    options = webdriver.ChromeOptions()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    options.headless = True
+    main_driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+
     main_driver.get(getVideoUrl(query))
     sleep(0.2)
     soup = BeautifulSoup(main_driver.page_source, "lxml")
     videos = soup.find_all("a", id="thumbnail")
-    for i in range(0, count):
-        print("https://www.youtube.com" + videos[i]["href"])
-    return
 
-video_search("")
+    res = []
+    for i in range(0, min(count, len(videos))):
+        try:
+            res.append("https://www.youtube.com" + videos[i]["href"])
+        except:
+            pass
 
-main_driver.quit()
+    main_driver.quit()
+
+    return res
+
+#print(video_search("Котики"))
